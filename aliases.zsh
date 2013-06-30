22ee9f0c3cd029d778a531624b110f2369921f75
Alex Landovskis <alex@landovskis.com>
2013-06-30 16:45:37 -0400

    Remove Concordia-specific aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -21,17 +21,6 @@ alias pingr="ping -c 4 192.168.100.1"
 alias pingns="ping -c 4 8.8.8.8; ping -c 4 8.8.4.4"
 alias pinga="pingr && pingns && pingg"
 
-alias cu="cd $CONCORDIA_DIR"
-alias sshl="ssh j_landov@login.encs.concordia.ca"
-alias c476="cd $COMP476_DIR"
-alias e393="cd $ENCS393_DIR"
-alias s422="cd $SOEN422_DIR"
-alias s422proj="cd $SOEN422_PROJECT_DIR"
-alias serteensy="minicom -D /dev/tty.usbmodem12341"
-alias serbonea="minicom -D /dev/tty.usbserial-TIVHL2KQA"
-alias serboneb="minicom -D /dev/tty.usbserial-TIVHL2KQB"
-alias xbee="minicom -D /dev/tty.usbserial-A1011FEB -b 9600"
-
 # Interests
 alias urbs="cd $URBANISM_DIR"
 alias sshr="ssh vyatta@router"
