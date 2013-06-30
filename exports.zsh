16d657ae932c632a2296645bca42870e7ba9a0bf
Alex Landovskis <alex@landovskis.com>
2013-06-30 16:30:19 -0400

    Move exports into a separate file.

---
+++ exports.zsh
@@ -0,0 +1,35 @@
+
+export PATH=/usr/local/texlive/2012/bin/universal-darwin:/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/teensy.app/Contents/MacOS:/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+# -------------
+# | Variables |
+# -------------
+# General
+export DOCUMENT_DIR="$HOME/Documents"
+export MOVIE_DIR="$HOME/Movies"
+export IMAGE_DIR="$HOME/Photos"
+export MUSIC_DIR="$HOME/Music"
+export SITE_DIR="$HOME/Sites"
+export DOWNLOAD_DIR="$HOME/Downloads"
+export APPLICATION_DIR="/Applications"
+export DROPBOX_DIR="$HOME/Dropbox"
+
+# Concordia
+export CONCORDIA_DIR="$DROPBOX_DIR/concordia"
+export COMP476_DIR="$CONCORDIA_DIR/winter_2013/comp476"
+export ENCS393_DIR="$CONCORDIA_DIR/winter_2013/encs393"
+export SOEN422_DIR="$CONCORDIA_DIR/winter_2013/soen422"
+export SOEN422_PROJECT_DIR="$SOEN422_DIR/teensyawesome/project"
+
+# Interests
+export URBANISM_DIR="$DROPBOX_DIR/urbanism"
+
+# Programming
+export PYTHON_DIR="$DROPBOX_DIR/python"
+export RUBY_DIR="$DROPBOX_DIR/ruby"
+export RAILS_DIR="$RUBY_DIR/rails"
+
+# Use emacsclient as the default editor.
+export EDITOR=/bin/edit
