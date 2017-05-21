bfebaecdf56fd95ddbceb933967d8c9b24c33b6d
Alex Landovskis <alex@landovskis.com>
2017-05-21 16:53:28 -0400

    snapshot-ls: Add script to list snapshots.

---
+++ bin/snapshot-ls.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+IFS=$'\n\t'
+
+DOTFILE="${HOME}/.snapshot"
+
+usage()
+{
+    echo "usage: $0 TARGET"
+}
+
+TARGET=${1:-}
+if [[ -z "${TARGET}" ]]; then
+    usage
+    exit 1
+fi
+
+if [ ! -r "${DOTFILE}" ]; then
+    echo "${DOTFILE} was not found."
+    exit 2
+fi
+
+source ${DOTFILE}
+SNAPSHOT_DIR=${SNAPSHOT_DIR:-}
+if [[ -z "$SNAPSHOT_DIR" ]]; then
+    echo "SNAPSHOT_DIR not found in ${DOTFILE}"
+    exit 3
+fi
+
+SNAPSHOT_PATH="${SNAPSHOT_DIR}/${TARGET}"
+
+if [ ! -d "${SNAPSHOT_PATH}" ]; then
+    echo "No snapshots found for ${TARGET}"
+    exit 0
+fi
+
+ls ${SNAPSHOT_PATH} | sed 's/\.tar\..*//g'
