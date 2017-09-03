91d3bb2f44da474f6e956e7fa8047e3a190e089f
Alex Landovskis <alex@landovskis.com>
2017-09-03 12:34:24 -0400

    aliases.sh: Remove some old aliases.

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,9 +6,7 @@ alias doc="cd $DOCUMENT_DIR"
 alias mov="cd $MOVIE_DIR"
 alias img="cd $IMAGE_DIR"
 alias mus="cd $MUSIC_DIR"
-alias sit="cd $SITE_DIR"
 alias dow="cd $DOWNLOAD_DIR"
-alias dbox="cd $DROPBOX_DIR"
 
 # ls
 alias ls=" ls"
@@ -20,6 +18,3 @@ alias pingg="ping -c 4 google.com"
 alias pingr="ping -c 4 192.168.100.1"
 alias pingns="ping -c 4 8.8.8.8; ping -c 4 8.8.4.4"
 alias pinga="pingr && pingns && pingg"
-
-# Interests
-alias urbs="cd $URBANISM_DIR"
